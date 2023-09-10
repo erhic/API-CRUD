7e05317c1b2f8d97c9f2ffec602a59963b9d95ab
erhic <ericngugi24@gmail.com>
2023-09-10 11:09:52 +0300

    feat: add function to interact with db by adding single item, increment db items length by index ,to position new item then pass rest of the data using spread operator ,case of  error to be logged

--- pets/models/pets.model.js
+++ pets/models/pets.model.js
@@ -1,7 +1,25 @@
 import db from "../../db/db.js";
 
-export const addItem = () => {};
-export const listItems = () => {};
-export const getItem = () => {};
-export const deleteItem = () => {};
-export const editItem = () => {};
+export const addItem = (data) => {
+  try {
+    const petItem = { id: db.pets.length + 1, ...data };
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+export const listItems = () => {
+  try {
+  } catch (error) {}
+};
+export const getItem = (id) => {
+  try {
+  } catch (error) {}
+};
+export const deleteItem = (id) => {
+  try {
+  } catch (error) {}
+};
+export const editItem = (data) => {
+  try {
+  } catch (error) {}
+};
