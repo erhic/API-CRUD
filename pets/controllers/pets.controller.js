e7857dd9c3bea56c28f27796de10dd4d4016c038
erhic <ericngugi24@gmail.com>
2023-09-10 09:59:01 +0300

    feat: import function from models   to control the response that will be distributed upon each reource request

--- pets/controllers/pets.controller.js
+++ pets/controllers/pets.controller.js
@@ -1,5 +1,31 @@
-export const addPet = () => {};
-export const listPets = () => {};
-export const getPet = () => {};
-export const editPet = () => {};
-export const deletePet = () => {};
+import {
+  addItem,
+  listItems,
+  getItem,
+  editItem,
+  deleteItem,
+} from "../models/pets.model";
+/**
+ * callback function to call our models function ,that return resources from the database,
+ */
+
+export const addPet = (req, res) => {
+  try {
+  } catch (error) {}
+};
+export const listPets = (req, res) => {
+  try {
+  } catch (error) {}
+};
+export const getPet = (req, res) => {
+  try {
+  } catch (error) {}
+};
+export const editPet = (req, res) => {
+  try {
+  } catch (error) {}
+};
+export const deletePet = (req, res) => {
+  try {
+  } catch (error) {}
+};
