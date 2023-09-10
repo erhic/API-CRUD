2e506aa9e414ac937507eaddd378993dc6a70320
erhic <ericngugi24@gmail.com>
2023-09-10 08:34:18 +0300

    feat: add method to use to perform CRUD or read , delete and updated data in the database

--- pets/routes/pets.routes.js
+++ pets/routes/pets.routes.js
@@ -4,3 +4,8 @@ import express from "express";
 added a router function that accepts router options to pass our routes
 */
 const router = express.Router();
+
+router.get();
+router.post();
+router.put();
+router.delete();
