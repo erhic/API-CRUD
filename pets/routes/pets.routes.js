8365e0c478a09db68d10260375e8b25e242b69a8
erhic <ericngugi24@gmail.com>
2023-09-10 09:24:52 +0300

    feat: import variables to trigger callback function in controller, when various end points are passed

--- pets/routes/pets.routes.js
+++ pets/routes/pets.routes.js
@@ -1,4 +1,11 @@
 import express from "express";
+import {
+  addPet,
+  getPet,
+  listPets,
+  editPet,
+  deletePet,
+} from "../controllers/pets.controller.js";
 
 /**  
 added a router function that accepts router options to pass our routes
@@ -7,8 +14,8 @@ const router = express.Router();
 /**
  * Action that can be performed entails , adding a pet , editing/updating the pet ,deleting the pet ,displaying a pet ,showing pets
  */
-router.get("/");
-router.get("/:id");
-router.put("/:id");
-router.post("/");
-router.delete("/:id");
+router.post("/", addPet);
+router.get("/", listPets);
+router.get("/:id", getPet);
+router.put("/:id", editPet);
+router.delete("/:id", deletePet);
