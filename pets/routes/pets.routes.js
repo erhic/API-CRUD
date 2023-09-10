348cf38bbc934867ef1e93add55cb641e86fbdbf
erhic <ericngugi24@gmail.com>
2023-09-10 08:45:23 +0300

    feat: add routes param for various routes

--- pets/routes/pets.routes.js
+++ pets/routes/pets.routes.js
@@ -4,8 +4,11 @@ import express from "express";
 added a router function that accepts router options to pass our routes
 */
 const router = express.Router();
-
-router.get();
-router.post();
-router.put();
-router.delete();
+/**
+ * Action that can be performed entails , adding a pet , editing/updating the pet ,deleting the pet ,displaying a pet ,showing pets
+ */
+router.get("/");
+router.get("/:id");
+router.put("/:id");
+router.post("/");
+router.delete("/:id");
