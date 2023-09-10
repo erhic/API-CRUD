979c9ada0e2eac9e6466bcc60df210263be73a73
erhic <ericngugi24@gmail.com>
2023-09-10 10:45:14 +0300

    feat: add function to edit single item ,required item id passed as param after conversion to integer and body content ,status code  200 ok,500 for err

--- pets/controllers/pets.controller.js
+++ pets/controllers/pets.controller.js
@@ -38,7 +38,11 @@ export const getPet = (req, res) => {
 
 export const editPet = (req, res) => {
   try {
-  } catch (error) {}
+    const resp = editItem(parseInt(req.params.id), req.body);
+    res.status(200).json(resp);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 export const deletePet = (req, res) => {
