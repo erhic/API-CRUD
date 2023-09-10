48729004a9c39ed57001b7e16b8d939bc8d31313
erhic <ericngugi24@gmail.com>
2023-09-10 09:40:06 +0300

    feat: add functions that purpose is to get resources from the database , exported them as variables

---
+++ pets/models/pets.model.js
@@ -0,0 +1,7 @@
+import db from "../../db/db.js";
+
+export const addItem = () => {};
+export const listItems = () => {};
+export const getItem = () => {};
+export const deleteItem = () => {};
+export const editItem = () => {};
