dc4deca3bd63ef3c946a3ff72f3035b1b2c24428
erhic <ericngugi24@gmail.com>
2023-09-10 07:51:22 +0300

    feat: creating application layer of our application and getting rid of imported top function from express package to use it create our express app.

--- app.js
+++ app.js
@@ -2,3 +2,13 @@
  */
 import cors from "cors";
 import express from "express";
+
+/**
+ * importing route endpoints entry point directory for configurations
+ *  */
+import petRoutes from "./pets/routes/pets.routes.js";
+
+/**
+ * getting rid of express top level function from imported express be able to use it to create an express app , we store it in a variable.
+ */
+const app = express();
